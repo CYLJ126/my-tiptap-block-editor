869628a64e5e63bb270b47b925ec03131ca3c91c
cylj126 <1586990974@qq.com>
2026-05-16 21:48:41 +0800

    feat: 项目NobodyIsPerfect-007-RichText-调整为 deepseek 模型

--- src/components/editor/utilities/ai-adapter.ts
+++ src/components/editor/utilities/ai-adapter.ts
@@ -2,8 +2,8 @@ import { createDeepSeek } from '@ai-sdk/deepseek';
 import { streamText } from "ai";
 
 export default async function generateAiResponse({ prompt }: { prompt: string; }) {
-  // const apiKey = import.meta.env.DEEPSEEK_API_KEY;
-  const apiKey = import.meta.env.DEEPSEEK_API_KEY;
+  // const apiKey = import.meta.env.VITE_DEEPSEEK_API_KEY;
+  const apiKey = import.meta.env.VITE_DEEPSEEK_API_KEY;
   console.log('meta.env: ', import.meta.env);
   if (!apiKey) {
     throw Error("Require deepseek api key");
